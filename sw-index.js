4331e4ac09d35c60e96606059c18a04ff8a34516
jeancarlozelaya <jz.92@live.com>
2026-09-16 10:47:49 -0600

    Update sw-index.js

--- sw-index.js
+++ sw-index.js
@@ -1,6 +1,6 @@
 // Archivo: sw-index.js
 
-const CACHE_NAME_INDEX = 'pibrisa-index-v8.2.1';
+const CACHE_NAME_INDEX = 'pibrisa-index-v8.2.2';
 const urlsToCacheIndex = [
     './',
     './index.html',
